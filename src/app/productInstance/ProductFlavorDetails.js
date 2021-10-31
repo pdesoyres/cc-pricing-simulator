3206f11d25961de86ed1d75fdacf5af4a9bb5c4a
pierre <pierre.desoyres@enghouse.com>
2021-10-31 23:27:22 +0100

    Take profit of the KeyValuePairs component in the ProductFlavorDetails component

--- src/app/productInstance/ProductFlavorDetails.js
+++ src/app/productInstance/ProductFlavorDetails.js
@@ -1,9 +1,10 @@
 import './ProductFlavorDetails.css';
 import { classnames } from '../../utils/classnames';
+import { KeyValuePairs, KeyValuePair } from '../../components/KeyValuePairs';
 import { Numeric } from '../../components/Numeric';
 
 /**
- * Displays the details of a product flavor:
+ * Displays the details of a product flavor using {@link KeyValuePairs} component:
  * <ul>
  *   <li>Number of CPUs
  *   <li>Number of GPUs
@@ -15,13 +16,16 @@ import { Numeric } from '../../components/Numeric';
  * @return {JSX.Element}
  * @constructor
  */
-export const ProductFavorDetails = ({
+export const ProductFlavorDetails = ({
   productFlavor,
   className,
 }) => {
   return <div className={classnames('tp-product-flavor-details', className)}>
-    <div className='tp-product-flavor-details-label'>CPUs</div>
-    <div className='tp-product-flavor-details-value'><Numeric value={productFlavor.cpus}/></div>
+    <KeyValuePairs>
+      <KeyValuePair pKey="CPUs" value={<Numeric value={productFlavor.cpus}/>}/>
+      <KeyValuePair pKey="GPUs" value={<Numeric value={productFlavor.gpus}/>}/>
+      <KeyValuePair pKey="Memory" value={<Numeric value={productFlavor.mem} precision={2} unit="MiB"/>}/>
+    </KeyValuePairs>
 
     <div className='tp-product-flavor-details-label'>GPUs</div>
     <div className='tp-product-flavor-details-value'><Numeric value={productFlavor.gpus}/></div>
@@ -29,4 +33,4 @@ export const ProductFavorDetails = ({
     <div className='tp-product-flavor-details-label'>Memory</div>
     <div className='tp-product-flavor-details-value'><Numeric value={productFlavor.mem} precision={2} unit='MiB'/></div>
   </div>;
-}
+};
