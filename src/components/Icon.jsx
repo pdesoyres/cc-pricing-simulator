37ddd22e41efa2553d6393a90713e7e81b743600
pierre <pierre.desoyres@enghouse.com>
2021-10-31 18:25:17 +0100

    Enhance accessibility: - Add aria-label to svg icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -4,5 +4,5 @@ export const Icon = ({
   color
 }) => {
   const Svg = svg;
-  return <Svg className="cc-icon" width={size} height={size} fill={color} />
+  return <Svg className="cc-icon" width={size} height={size} fill={color} aria-label={svg.render?.name}/>
 };
